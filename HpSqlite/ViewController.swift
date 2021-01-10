d9330e8e3e61eb4c1e5e29dbf43b920e45e07cc3
huangpu0 <huangpu0205@163.com>
2021-01-10 12:36:33 +0800

    初始化

--- HpSqlite/ViewController.swift
+++ HpSqlite/ViewController.swift
@@ -11,9 +11,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let btn1 = UIButton.init()
+        btn1.frame = CGRect.init(x: 10, y: 100, width: 80, height: 80)
+        btn1.backgroundColor = .red
+        btn1.setTitle("增", for: .normal)
+        btn1.addTarget(self, action: #selector(creatEvent(_:)), for: .touchUpInside)
+        self.view.addSubview(btn1)
+        
+        let btn2 = UIButton.init()
+        btn2.frame = CGRect.init(x: 100, y: 100, width: 80, height: 80)
+        btn2.backgroundColor = .red
+        btn2.setTitle("删", for: .normal)
+        btn2.addTarget(self, action: #selector(deleteEvent(_:)), for: .touchUpInside)
+        self.view.addSubview(btn2)
+        
+        let btn3 = UIButton.init()
+        btn3.frame = CGRect.init(x: 190, y: 100, width: 80, height: 80)
+        btn3.backgroundColor = .red
+        btn3.setTitle("改", for: .normal)
+        btn3.addTarget(self, action: #selector(updateEvent(_:)), for: .touchUpInside)
+        self.view.addSubview(btn3)
+        
+        let btn4 = UIButton.init()
+        btn4.frame = CGRect.init(x: 280, y: 100, width: 80, height: 80)
+        btn4.backgroundColor = .red
+        btn4.setTitle("查", for: .normal)
+        btn4.addTarget(self, action: #selector(queryEvent(_:)), for: .touchUpInside)
+        self.view.addSubview(btn4)
     }
 
+    /// 增
+    @objc
+    func creatEvent(_ btn: UIButton) -> Void {
+        
+    }
+    
+    /// 删
+    @objc
+    func deleteEvent(_ btn: UIButton) -> Void {
+        
+    }
+    
+    /// 改
+    @objc
+    func updateEvent(_ btn: UIButton) -> Void {
+        
+    }
+    
+    /// 查
+    @objc
+    func queryEvent(_ btn: UIButton) -> Void {
+        
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+
+target 'HpSqlite' do
+use_frameworks!
+
+pod 'SQLite.swift', '~> 0.12.2'
+
+end
